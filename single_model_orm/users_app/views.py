fe10e3353b383da53403e3bc4be7c639c9c251ae
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-09 18:28:44 -0700

    Need to update data base

--- single_model_orm/users_app/views.py
+++ single_model_orm/users_app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def process(requst):
+    User.objects.create(
+        first_name=request.POST["first_name"],
+        last_name=request.POST["last_name"],
+        email=request.POST["email_address"],
+        age=request.POST["age"],
+    )
+    return redirect('/')
