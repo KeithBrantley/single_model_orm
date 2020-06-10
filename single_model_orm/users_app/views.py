8c53527c0d1967c77427dd79ac0924e27d7e7cf7
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-10 00:03:10 -0700

    need to delete

--- single_model_orm/users_app/views.py
+++ single_model_orm/users_app/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'all_users': User.objects.all()
+    }
+    return render(request, 'index.html',context)
 
-def process(requst):
+def process(request):
+    print(request.POST)
     User.objects.create(
         first_name=request.POST["first_name"],
         last_name=request.POST["last_name"],
         email=request.POST["email_address"],
-        age=request.POST["age"],
+        age=request.POST["age"]
     )
     return redirect('/')
